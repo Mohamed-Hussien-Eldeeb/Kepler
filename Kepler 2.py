1742b9dee628392b3d2515471240afea68ca7fb1
Mohamed-Hussien-Eldeeb <74769984+Mohamed-Hussien-Eldeeb@users.noreply.github.com>
2021-03-13 20:53:03 +0200

    Add files via upload

---
+++ Kepler 2.py
@@ -0,0 +1,49 @@
+from math import *
+import numpy as np
+import pygame
+
+
+def position(e, T, r_0):
+    E = 0
+    out = []
+    acc = 1 / T
+    for t in np.arange(0, T, acc):
+        M = 2 * pi * t / T
+        E -= (E - e * sin(E) - M) / (1 - e * cos(E))
+        V = 2 * atan(sqrt((1 + e) / (1 - e)) * tan(E / 2))
+        r = r_0 * (1 - e ** 2) / (1 + e * cos(V))
+        out.append([r, V])
+    return out
+
+
+running = True
+pygame.init()
+window = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption('Kepler')
+clock = pygame.time.Clock()
+pos = 0
+ro = 0
+font = pygame.font.Font('freesansbold.ttf', 20)
+while running:
+    window.fill('black')
+    text = font.render(f'e = {round(ro, 2)}', True, (255, 255, 255))
+    textRect = text.get_rect()
+    textRect.center = (200, 800)
+    window.blit(text, textRect)
+    motion = position(ro, 5, 250)
+    for j in position(ro, 50, 250):
+        pygame.draw.circle(window, 'white', [j[0] * cos(j[1]) + 500, j[0] * sin(j[1]) + 500], 1)
+    for i in motion:
+        pygame.draw.circle(window, 'white', [i[0] * cos(i[1]) + 500, i[0] * sin(i[1]) + 500], 1)
+        pygame.draw.line(window, 'white', [500, 500], [i[0] * cos(i[1]) + 500, i[0] * sin(i[1]) + 500], 1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+    clock.tick(60)
+    pos += 1
+    pos %= len(motion)
+    ro += 0.001
+    ro %= 0.8
+pygame.quit()
+quit()
